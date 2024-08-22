fa59a3c511adad5f4cb37d0ce660aefa392a5694
dmitriy <kylishovdimka@ukr.net>
2024-08-22 18:08:49 +0300

    add forms/admin in app smart_test

--- src/smart_test/admin.py
+++ src/smart_test/admin.py
@@ -1,3 +1,40 @@
-from django.contrib import admin # noqa
+from django.contrib import admin
+
+from smart_test.forms import QuestionsInlineFormSet, AnswerInlineFormSet
+from smart_test.models import TestResult, Answer, Question, Test, Topic
 
 # Register your models here.
+
+
+class AnswerInline(admin.TabularInline):
+    model = Answer
+    fields = ('text', 'is_correct')
+    show_change_link = True
+    extra = 0
+    formset = AnswerInlineFormSet
+
+
+class QuestionAdminModel(admin.ModelAdmin):
+    inlines = (AnswerInline, )
+
+
+class QuestionInline(admin.TabularInline):
+    model = Question
+    fields = ('text', 'order_number')
+    show_change_link = True
+    extra = 0
+    formset = QuestionsInlineFormSet
+    ordering = ('order_number', )
+
+
+class TestAdminModel(admin.ModelAdmin):
+
+    list_per_page = 10
+    inlines = (QuestionInline, )
+
+
+admin.site.register(Topic)
+admin.site.register(Test, TestAdminModel)
+admin.site.register(Question, QuestionAdminModel)
+admin.site.register(Answer)
+admin.site.register(TestResult)
---
+++ src/smart_test/forms.py
@@ -0,0 +1,31 @@
+from django.forms import BaseInlineFormSet
+from django.core.exceptions import ValidationError
+
+
+class QuestionsInlineFormSet(BaseInlineFormSet):
+
+    def clean(self):
+        if not (self.instance.QUESTION_MIN_LIMIT <= len(self.forms) <= self.instance.QUESTION_MAX_LIMIT):
+            raise ValidationError(
+                "Quantity of Question is out of range ({}..{})".format(self.instance.QUESTION_MIN_LIMIT,
+                                                                       self.instance.QUESTION_MAX_LIMIT))
+
+
+class AnswerInlineFormSet(BaseInlineFormSet):
+
+    def clean(self):
+        if not (self.instance.ANSWER_MIN_LIMIT <= len(self.forms) <= self.instance.ANSWER_MAX_LIMIT):
+            raise ValidationError(
+                "Quantity of Question is out of range ({}..{})".format(self.instance.ANSWER_MIN_LIMIT,
+                                                                       self.instance.ANSWER_MAX_LIMIT))
+
+        num_correct_answers = sum([
+            1 for form in self.forms
+            if form.cleaned_date["is_correct"]
+        ])
+
+        if num_correct_answers == 0:
+            raise ValidationError("At LEAST one answer must be correct!")
+
+        if num_correct_answers == len(self.forms):
+            raise ValidationError("Not allowed to select ALL answers!")
