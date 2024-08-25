0372e52bb232ff626437f8be2304739cacce2206
dmitriy <kylishovdimka@ukr.net>
2024-08-25 23:06:45 +0300

    fix tests

---
+++ src/smart_test/tests/test_urls.py
@@ -0,0 +1,5 @@
+from unittest import TestCase
+
+
+class TestUrls(TestCase):
+    pass
--- src/tests/test_dev.py
+++
@@ -1,7 +0,0 @@
-from unittest import TestCase
-
-
-class TestBasic(TestCase):
-
-    def tests_add(self):
-        pass
